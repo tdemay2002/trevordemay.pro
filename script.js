9eb0fe92648fc0055c3ddff6afdea829e4c1a823
Trevor DeMay <157247545+tdemay2002@users.noreply.github.com>
2024-01-21 18:29:26 -0500

    test

--- script.js
+++ script.js
@@ -2,6 +2,5 @@ function myFunction(){
     document.getElementById("panda_image").style.display = "none";
     document.getElementById("the_surprise").style.display = "block";
     document.getElementById("the_surprise").src = "https://www.youtube.com/embed/dQw4w9WgXcQ?si=f9btx_hn9PtOrB9t&amp;controls=0?autoplay=1";
-    document.getElementById("the_surprise").autoplay
     document.getElementById("changing_text").innerText = "Thanks!!!"
 };
