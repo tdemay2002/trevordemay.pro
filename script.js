2d096c4b84bd83e1d08f6e80668ba47f005079b5
Trevor DeMay <157247545+tdemay2002@users.noreply.github.com>
2024-01-21 18:27:35 -0500

    autoplay is enabled

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function myFunction(){
     document.getElementById("panda_image").style.display = "none";
     document.getElementById("the_surprise").style.display = "block";
-    document.getElementById("the_surprise").src = "https://www.youtube.com/embed/dQw4w9WgXcQ?si=f9btx_hn9PtOrB9t&amp;controls=0";
+    document.getElementById("the_surprise").src = "https://www.youtube.com/embed/dQw4w9WgXcQ?si=f9btx_hn9PtOrB9t&amp;controls=0?autoplay=1";
+    document.getElementById("the_surprise").autoplay
     document.getElementById("changing_text").innerText = "Thanks!!!"
 };
