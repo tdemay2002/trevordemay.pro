b6b96d6a4d1b0cbc82d22c04233cab91f1359fd2
Trevor DeMay <157247545+tdemay2002@users.noreply.github.com>
2024-01-21 20:59:55 -0500

    forgot something

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function myFunction(){
     document.getElementById("changing_text").innerText = "Thanks!!!";
     document.getElementById("panda_image").style.display = "none";
     document.getElementById("the_surprise").height = "315";
-    
-    var iframe = document.getElementById('myIframe');
+
+    var iframe = document.getElementById('the_surprise');
     var currentSrc = iframe.src;
     var hasAutoplay = currentSrc.includes('autoplay=1');
     if (!hasAutoplay) {
