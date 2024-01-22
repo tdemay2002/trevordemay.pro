5c2b1aeef7cbb50ede87266f6e24e69b45c27d79
Trevor DeMay <157247545+tdemay2002@users.noreply.github.com>
2024-01-21 20:54:31 -0500

    using youtube api

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function myFunction(){
+    document.getElementById("changing_text").innerText = "Thanks!!!";
     document.getElementById("panda_image").style.display = "none";
     document.getElementById("the_surprise").height = "315";
-    document.getElementById("the_surprise").play();
-    document.getElementById("changing_text").innerText = "Thanks!!!"
+    document.getElementById("the_surprise").playVideo();
 };
