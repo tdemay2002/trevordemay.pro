c6c828fd291cb363134e1f41cd8010cee854c647
Trevor DeMay <157247545+tdemay2002@users.noreply.github.com>
2024-10-05 23:31:39 -0400

    centered logo text and optimized css a bit. deleted stray code.

--- script.js
+++ script.js
@@ -4,10 +4,8 @@ function toggleNav(){
     const hamburger = document.getElementById('mobile-menu');
     const navLinks = document.querySelector('.nav-links');
     const navLogo = document.querySelector('.nav-logo');
-    const navBar = document.querySelector('navBar');
 
     hamburger.classList.toggle('active');
     navLinks.classList.toggle('active');
     navLogo.classList.toggle('active');
-    navBar.classList.toggle('active');
 }
