fa2f8854f3197b4080dc7d2691d8378ca9ffa3b9
Trevor DeMay <157247545+tdemay2002@users.noreply.github.com>
2024-10-05 22:15:39 -0400

    optimized navigation bar. added animation and funtionality to hamburger menu

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 document.getElementById('mobile-menu').addEventListener('click', function(){
     const hamburger = document.getElementById('mobile-menu');
     const navLinks = document.querySelector('.nav-links');
+    const navLogo = document.querySelector('.nav-logo');
+    const navBar = document.querySelector('navBar');
 
     hamburger.classList.toggle('active');
     navLinks.classList.toggle('active');
+    navLogo.classList.toggle('active');
+    navBar.classList.toggle('active');
 });
